a6314426485f288011213d6f8e0c0c94f0f33828
ermu2001 <ermuzzz2001@gmail.com>
2024-11-25 22:51:21 -0500

    added paper's tome

--- scripts/inference/benchmark_video_generation.sh
+++ scripts/inference/benchmark_video_generation.sh
@@ -6,7 +6,7 @@ benchmark_origin () {
 
 benchmark_naive_tome () {
     echo "Benchmarking video generation with text-to-video models"
-    python -m accediff.inference.generate_video_text_to_video --config-name config_text_to_video_tome
+    python -m accediff.inference.generate_video_text_to_video --config-name config_text_to_video_tome $@
 }
 
 
