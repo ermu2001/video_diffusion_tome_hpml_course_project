1af724c1c2cfbfacbfe22ed125118c4317f982eb
ermu2001 <ermuzzz2001@gmail.com>
2024-11-27 23:46:11 -0500

    added training midjourney-v6

--- accediff/train/lcm_sd35_lora.py
+++ accediff/train/lcm_sd35_lora.py
@@ -16,8 +16,6 @@
 import copy
 import functools
 import gc
-import itertools
-import json
 import logging
 import math
 import os
@@ -32,26 +30,16 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
-import torchvision.transforms.functional as TF
 import transformers
 import webdataset as wds
 from accelerate import Accelerator
 from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
-from braceexpand import braceexpand
 from huggingface_hub import create_repo, upload_folder
 from packaging import version
 from peft import LoraConfig, get_peft_model, get_peft_model_state_dict
-from torch.utils.data import default_collate
-from torchvision import transforms
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer, CLIPTextModel, PretrainedConfig
-from webdataset.tariterators import (
-    base_plus_ext,
-    tar_file_expander,
-    url_opener,
-    valid_sample,
-)
 
 import diffusers
 from diffusers import (
@@ -63,7 +51,7 @@ from diffusers import (
     DDPMScheduler,
 )
 from diffusers.optimization import get_scheduler
-from diffusers.training_utils import resolve_interpolation_mode, free_memory, compute_density_for_timestep_sampling
+from diffusers.training_utils import free_memory
 from diffusers.utils import check_min_version, is_wandb_available
 from diffusers.utils.import_utils import is_xformers_available
 
@@ -105,148 +93,6 @@ def get_module_kohya_state_dict(module, prefix: str, dtype: torch.dtype, adapter
             kohya_ss_state_dict[alpha_key] = torch.tensor(module.peft_config[adapter_name].lora_alpha).to(dtype)
 
     return kohya_ss_state_dict
-
-
-def filter_keys(key_set):
-    def _f(dictionary):
-        return {k: v for k, v in dictionary.items() if k in key_set}
-
-    return _f
-
-
-def group_by_keys_nothrow(data, keys=base_plus_ext, lcase=True, suffixes=None, handler=None):
-    """Return function over iterator that groups key, value pairs into samples.
-
-    :param keys: function that splits the key into key and extension (base_plus_ext) :param lcase: convert suffixes to
-    lower case (Default value = True)
-    """
-    current_sample = None
-    for filesample in data:
-        assert isinstance(filesample, dict)
-        fname, value = filesample["fname"], filesample["data"]
-        prefix, suffix = keys(fname)
-        if prefix is None:
-            continue
-        if lcase:
-            suffix = suffix.lower()
-        # FIXME webdataset version throws if suffix in current_sample, but we have a potential for
-        #  this happening in the current LAION400m dataset if a tar ends with same prefix as the next
-        #  begins, rare, but can happen since prefix aren't unique across tar files in that dataset
-        if current_sample is None or prefix != current_sample["__key__"] or suffix in current_sample:
-            if valid_sample(current_sample):
-                yield current_sample
-            current_sample = {"__key__": prefix, "__url__": filesample["__url__"]}
-        if suffixes is None or suffix in suffixes:
-            current_sample[suffix] = value
-    if valid_sample(current_sample):
-        yield current_sample
-
-
-def tarfile_to_samples_nothrow(src, handler=wds.warn_and_continue):
-    # NOTE this is a re-impl of the webdataset impl with group_by_keys that doesn't throw
-    streams = url_opener(src, handler=handler)
-    files = tar_file_expander(streams, handler=handler)
-    samples = group_by_keys_nothrow(files, handler=handler)
-    return samples
-
-
-class WebdatasetFilter:
-    def __init__(self, min_size=1024, max_pwatermark=0.5):
-        self.min_size = min_size
-        self.max_pwatermark = max_pwatermark
-
-    def __call__(self, x):
-        try:
-            if "json" in x:
-                x_json = json.loads(x["json"])
-                filter_size = (x_json.get("original_width", 0.0) or 0.0) >= self.min_size and x_json.get(
-                    "original_height", 0
-                ) >= self.min_size
-                filter_watermark = (x_json.get("pwatermark", 1.0) or 1.0) <= self.max_pwatermark
-                return filter_size and filter_watermark
-            else:
-                return False
-        except Exception:
-            return False
-
-
-class SDText2ImageDataset:
-    def __init__(
-        self,
-        train_shards_path_or_url: Union[str, List[str]],
-        num_train_examples: int,
-        per_gpu_batch_size: int,
-        global_batch_size: int,
-        num_workers: int,
-        resolution: int = 512,
-        interpolation_type: str = "bilinear",
-        shuffle_buffer_size: int = 1000,
-        pin_memory: bool = False,
-        persistent_workers: bool = False,
-    ):
-        if not isinstance(train_shards_path_or_url, str):
-            train_shards_path_or_url = [list(braceexpand(urls)) for urls in train_shards_path_or_url]
-            # flatten list using itertools
-            train_shards_path_or_url = list(itertools.chain.from_iterable(train_shards_path_or_url))
-
-        interpolation_mode = resolve_interpolation_mode(interpolation_type)
-
-        def transform(example):
-            # resize image
-            image = example["image"]
-            image = TF.resize(image, resolution, interpolation=interpolation_mode)
-
-            # get crop coordinates and crop image
-            c_top, c_left, _, _ = transforms.RandomCrop.get_params(image, output_size=(resolution, resolution))
-            image = TF.crop(image, c_top, c_left, resolution, resolution)
-            image = TF.to_tensor(image)
-            image = TF.normalize(image, [0.5], [0.5])
-
-            example["image"] = image
-            return example
-
-        processing_pipeline = [
-            wds.decode("pil", handler=wds.ignore_and_continue),
-            wds.rename(image="jpg;png;jpeg;webp", text="text;txt;caption", handler=wds.warn_and_continue),
-            wds.map(filter_keys({"image", "text"})),
-            wds.map(transform),
-            wds.to_tuple("image", "text"),
-        ]
-
-        # Create train dataset and loader
-        pipeline = [
-            wds.ResampledShards(train_shards_path_or_url),
-            tarfile_to_samples_nothrow,
-            wds.shuffle(shuffle_buffer_size),
-            *processing_pipeline,
-            wds.batched(per_gpu_batch_size, partial=False, collation_fn=default_collate),
-        ]
-
-        num_worker_batches = math.ceil(num_train_examples / (global_batch_size * num_workers))  # per dataloader worker
-        num_batches = num_worker_batches * num_workers
-        num_samples = num_batches * global_batch_size
-
-        # each worker is iterating over this
-        self._train_dataset = wds.DataPipeline(*pipeline).with_epoch(num_worker_batches)
-        self._train_dataloader = wds.WebLoader(
-            self._train_dataset,
-            batch_size=None,
-            shuffle=False,
-            num_workers=num_workers,
-            pin_memory=pin_memory,
-            persistent_workers=persistent_workers,
-        )
-        # add meta-data to dataloader instance for convenience
-        self._train_dataloader.num_batches = num_batches
-        self._train_dataloader.num_samples = num_samples
-
-    @property
-    def train_dataset(self):
-        return self._train_dataset
-
-    @property
-    def train_dataloader(self):
-        return self._train_dataloader
 
 
 @torch.no_grad()
@@ -794,8 +640,8 @@ def train(args: DictConfig, accelerator: Accelerator) -> None:
             "negative_pooled_prompt_embeds": negative_pooled_prompt_embeds,
         }
 
-    
-    dataset = SDText2ImageDataset(
+    dataset_cls = hydra.utils.get_class(args.data.dataset_cls)
+    dataset = dataset_cls(
         train_shards_path_or_url=args.data.train_shards_path_or_url,
         num_train_examples=args.train.max_train_samples,
         per_gpu_batch_size=args.train.per_device_train_batch_size,
@@ -863,6 +709,7 @@ def train(args: DictConfig, accelerator: Accelerator) -> None:
     total_batch_size = args.train.per_device_train_batch_size * accelerator.num_processes * args.train.gradient_accumulation_steps
 
     logger.info("***** Running training *****")
+    logger.info(f"  Run saved to {os.getcwd()}/{args.status.output_dir}")
     logger.info(f"  Num batches each epoch = {train_dataloader.num_batches}")
     logger.info(f"  Num Epochs = {args.train.num_train_epochs}")
     logger.info(f"  Instantaneous batch size per device = {args.train.per_device_train_batch_size}")
