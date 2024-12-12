07632bd1c8dd6649a13127b8d283ca8cd64338e9
ermu2001 <ermuzzz2001@gmail.com>
2024-12-11 23:07:14 -0500

    code clean up

--- accediff/train/statics.py
+++
@@ -1,8 +0,0 @@
-import os
-import os.path as osp
-SCRIPT_DIR = osp.dirname(osp.realpath(__file__))
-REPO_ROOT = osp.dirname(osp.dirname(SCRIPT_DIR))
-# path to your own image root, or set up DATAS same like me.
-MID_JOURNEY_V6_IMAGE_ROOT=os.environ.get("MID_JOURNEY_V6_IMAGE_ROOT", osp.join(REPO_ROOT, "DATAS/midjourney-v6-images"))
-
-
--- scripts/train/lcm_sd2_lora.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=.:${PYTHONPATH}
-accelerate launch accediff/train/lcm_sd2_lora.py
